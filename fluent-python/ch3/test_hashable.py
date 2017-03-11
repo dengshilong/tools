eaf0ae93648fe72fa6955370e0955e2104bbfdca
dengshilong <dengshilong1988@gmail.com>
2017-03-11 13:57:18 +0800

    fix: add unhashable object

---
+++ fluent-python/ch3/test_hashable.py
@@ -0,0 +1,22 @@
+
+class UnHashable(object):
+    """
+    >>> a = UnHashable(3)
+    >>> b = UnHashable(3)
+    >>> a == b
+    True
+    """
+    def __init__(self, x):
+        self.x = x
+
+    def __eq__(self, other):
+        return self.x == other.x
+
+    def __hash__(self):
+        import random
+        return hash("{} {}".format(self.x, random.random()))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
