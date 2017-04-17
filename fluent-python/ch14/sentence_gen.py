84adf33262e9a8868165f48137d2052e843128f9
dengshilong <dengshilong1988@gmail.com>
2017-04-17 20:26:58 +0800

    docs: add tech blog

--- fluent-python/ch14/sentence_gen.py
+++ fluent-python/ch14/sentence_gen.py
@@ -29,9 +29,11 @@ class Sentence:
     """
     def __init__(self, text): 
         self.text = text
-        self.words = RE_WORD.findall(text) 
+        self.words = RE_WORD.findall(text)
+
     def __repr__(self):
         return 'Sentence(%s)' % reprlib.repr(self.text)
+
     def __iter__(self):
         for word in self.words:
             yield word 
