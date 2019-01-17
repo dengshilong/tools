7cff1285ad344d7f4af3f743ac34c1c426770d09
dengshilong <dengshilong1988@gmail.com>
2019-01-17 11:41:18 +0800

    docs: add Slices exercise

---
+++ go/go_exercises/slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+    board := make([][]uint8, dy)
+    for i := range board {
+        board[i] = make([]uint8, dx)
+        for j := range board[i] {
+            board[i][j] = uint8((i + j) * i * j)
+        }
+    }
+    return board
+}
+
+func main() {
+    pic.Show(Pic)
+}
+
